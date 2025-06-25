a227dff6d14806e6c7801894b7fb62776a175633
MohamedKassim <mohamedkassim354@gmail.com>
2025-06-25 22:28:56 +0300

    ✨ feat: enhance PersonalizeScreen with loading state management
    - Added loading state management using useState to handle user creation processes
    - Updated onSubmit and onSkip functions to reflect loading state during async operations
    - Disabled input and button while loading to prevent multiple submissions
    - Integrated loading indicator in the button for better user feedback

--- src/screens/PersonalizeScreen/index.tsx
+++ src/screens/PersonalizeScreen/index.tsx
@@ -1,5 +1,5 @@
 import {View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './style';
 
 import PrimaryText from '../../components/atoms/PrimaryText';
@@ -20,6 +20,7 @@ import {createUser} from '../../db/services/user';
 
 const PersonalizeScreen = () => {
   const colors = useThemeColors();
+  const [isLoading, setIsLoading] = useState(false);
   const {control, handleSubmit} = useForm<PersonalizationFormSchema>({
     resolver: zodResolver(personalizationFormSchema),
   });
@@ -27,22 +28,28 @@ const PersonalizeScreen = () => {
   // TODO: user can go back to the previous screen and recreate the user with the same data
   const onSubmit = async (data: PersonalizationFormSchema) => {
     try {
+      setIsLoading(true);
       await createUser({username: data.name, email});
       navigate('OnboardingScreen');
     } catch (error) {
       console.error('Error saving user data to Realm:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
   // TODO: how should we skip if the email is required in the db?
 
   const onSkip = async () => {
     try {
+      setIsLoading(true);
       await createUser({username: 'User', email: 'Test@gmail.com'});
       navigate('OnboardingScreen');
     } catch (error) {
       console.error('Error saving demo user data to Realm:', error);
+    } finally {
+      setIsLoading(false);
+      navigate('OnboardingScreen');
     }
-    navigate('OnboardingScreen');
   };
 
   return (
@@ -79,11 +86,13 @@ const PersonalizeScreen = () => {
           returnKeyType="done"
           textContentType="username"
           enablesReturnKeyAutomatically
+          editable={!isLoading}
           onSubmitEditing={handleSubmit(onSubmit)}
         />
       </View>
 
       <Button
+        loading={isLoading}
         onPress={handleSubmit(onSubmit)}
         title={'Continue'}
         style={styles.buttonContainer}
